2ff924415c5f4a334444f2e5c4110f68730472a7
DAVEOSH <davosh4u@gmail.com>
2024-01-31 15:54:58 +0100

    Fixed Menu for mobile

--- js/script.js
+++ js/script.js
@@ -5,11 +5,14 @@ function userScroll() {
 
         if (window.scrollY > 50) {
             navbar.classList.add('bg-dark');
+            navbar.classList.add('navbar-sticky');
         }
         else {
             navbar.classList.remove('bg-dark');
+            navbar.classList.remove('navbar-sticky');
         }
 
     });
 }
 document.addEventListener('DOMContentLoaded', userScroll);
+
